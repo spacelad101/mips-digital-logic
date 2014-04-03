947fae3b5563ab0e8fd53a5832e5588dc785b957
spacelad101 <noahburrell0@gmail.com>
2014-04-03 19:24:39 -0400

    Constructor Stuff

--- construct.cpp
+++ construct.cpp
@@ -1,9 +1,8 @@
 #include "registers.h"
 #include <iostream>
 
-Registers reg;
-
-int construct(){
+void construct(){
+    Registers reg;
     for(int n=0;n<8;n++){reg.pipe[n]=false;}
     for(int n=0;n<16;n++){reg.imm[n]=false;}
     for(int n=0;n<8;n++){reg.rsrc1[n]=false;}
@@ -11,5 +10,4 @@ int construct(){
     for(int n=0;n<8;n++){reg.rdest[n]=false;}
     for(int n=0;n<8;n++){reg.lo[n]=false;}
     for(int n=0;n<8;n++){reg.hi[n]=false;}
-    return 0;
 }
