a31a860e2dcca13ee3e3ec36d26b41d6ed0c50c1
spacelad101 <noahburrell0@gmail.com>
2014-04-03 18:59:43 -0400

    broken

---
+++ construct.cpp
@@ -0,0 +1,14 @@
+#include "registers.h"
+#include <iostream>
+
+Registers reg;
+
+int construct(){
+    for(int n=0;n<8;n++){reg.pipe[n]=false;}
+    for(int n=0;n<16;n++){reg.imm[n]=false;}
+    for(int n=0;n<8;n++){reg.rsrc1[n]=false;}
+    for(int n=0;n<8;n++){reg.rsrc2[n]=false;}
+    for(int n=0;n<8;n++){reg.rdest[n]=false;}
+    for(int n=0;n<8;n++){reg.lo[n]=false;}
+    for(int n=0;n<8;n++){reg.hi[n]=false;}
+}
--- registers.h
+++ registers.h
@@ -11,13 +11,4 @@ class Registers{
         bool lo[8];
         bool hi[8];
 };
-Registers::Registers(){
-    for(int n=0;n<8;n++){pipe[n]=false;}
-    for(int n=0;n<16;n++){imm[n]=false;}
-    for(int n=0;n<8;n++){rsrc1[n]=false;}
-    for(int n=0;n<8;n++){rsrc2[n]=false;}
-    for(int n=0;n<8;n++){rdest[n]=false;}
-    for(int n=0;n<8;n++){lo[n]=false;}
-    for(int n=0;n<8;n++){hi[n]=false;}
-}
 #endif
