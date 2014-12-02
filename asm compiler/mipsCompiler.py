3ae2f23b8d557a06fc4d910cedf4d968c1640a88
Nicholas Morey - Home <theyoungermorey@gmail.com>
2014-12-01 21:57:01 -0500

    Comment Support Added
    Add support for comments in the asm code. And fixed the jal & j checker.

--- asm compiler/mipsCompiler.py
+++ asm compiler/mipsCompiler.py
@@ -39,18 +39,21 @@ def splice_file_input(file_input):  # find comma placement within the current li
         if index > -1:  # if comment found
                 file_input = file_input[:index]  # remove comments from file line
 
+        # find commands based on spaces in the file line
         index = 0
         while index < len(file_input):  # Find spaces in the supplied string
                 index = file_input.find(' ', index)  # finds the desired character within the string, then places the place value at which the desired char was found in the 'index' var
                 if index == -1:  # if the desired char is not found, the '.find()' command returns -1, then breaks the while loop (to avoid an infinite loop)
                         break
-                # print("Found a space at %d" %(index))  # print location of ,s when found
-                space_place.append(index)  # adds the place value of the desired char in the comma_place array
+
+                if index != 0 and index < len(file_input):
+                        space_place.append(index)  # adds the place value of the desired char in the comma_place array
                 index += 1  # adds on to the place value of the desired char, so the next such for the char, starts after the initially found one. (to avoid an infinite loop of finding the initial desired char)
 
         tmp_array.append(file_input[:space_place[0]])  # Adds the encoder command the the tmp_array
         file_input = file_input[space_place[0] + 1:]  # Removes the encoder command from the file input, since it has already been spliced into the tmp_array
 
+        # find commands based on commas in the file line
         index = 0
         while index < len(file_input):  # Find commas in the supplied string
                 index = file_input.find(',', index)  # finds the desired character within the string, then places the place value at which the desired char was found in the 'index' var
@@ -62,15 +65,19 @@ def splice_file_input(file_input):  # find comma placement within the current li
                 
         comma_place.append(len(file_input))  # add the end of the file_input
         # num_commands = int(len(comma_place) + 1)  # Stores the number of commands found in current line of the file
-        
+
+        # Adds the commands to the tmp_array
         file_start = 0 
         for c in comma_place:  # for the number of commas + the end
                 tmp_array.append(file_input[file_start:c])  # add the single command to the commands list, c = the end of the command in the string
                 file_start = c + 1  # store the beginning of the next command's location in the string
 
-        commands.append(tmp_array)  # add the tmp_array to the master array
+        for c in range(0, len(tmp_array)):  # for the length of the array
+            index = tmp_array[c].find(' ')  # search for ' ' (spaces), if found note their index
+            if index != -1:  # if one is found
+                    tmp_array[c] = tmp_array[c].replace(' ', '')  # replace the space with nothing (removing it from the command)
 
-        # print (tmp_array)
+        commands.append(tmp_array)  # add the tmp_array to the master array, as a second dimension (representing a new line)
         return
 
 with open(file_name, 'r') as asmFile:  # Open file
@@ -80,64 +87,51 @@ with open(file_name, 'r') as asmFile:  # Open file
 
 # first part 'encoder' gets converted into binary via the 'opcode' dict.
 def opcode_conversion():
-        num_of_secondary_arrays = int(len(commands))  # Find the number of second dimension arrays in the master array
-        for c in range(0, num_of_secondary_arrays):  # for the number of secondary arrays in commands
+        for c in range(0, len(commands)):  # for the number of secondary arrays in commands
                 commands[c][0] = opcode[commands[c][0]]  # find encoder command (key) in dict, and replace with definition
         return
-          
-                                
+
+
 # the numbers following get converted into binary via the 'registers' array, number = registers[number]
 # encoders with 'i' at the end will have a hex value, hex value will always be the last value in the second dimensional array and start with '0x'
 # all register address start with '$'
 def to_binary_converter():
-        num_of_secondary_arrays = int(len(commands))  # holds number 
-        for c in range(0, num_of_secondary_arrays):  # for the number of secondary arrays in commands
+        for c in range(0, len(commands)):  # for the number of secondary arrays in commands
                 if commands[c][0] == 'j' or commands[c][0] == 'jal':  # If encoder is a jump instruction
-                        # print('J or JAL found. Checking')
-                        num_of_conversions = int(len(commands[c]))  # find the number of commands in the second dimension array
-                        for sc in range(0, num_of_conversions):  # runs for the number of commands in the second dimension array
+                        for sc in range(1, len(commands[c])):  # runs for the number of commands in the second dimension array (start at one to avoid conflict with the encoder command)
                                 # print('checking')
                                 index = commands[c][sc].find('0x')  # search for hex value formatting
                                 if index == -1:  # if none found display error message and end script
                                         print('Bad "' + commands[c][0] + '" formatting, on line ' + str(c + 1) + '. "' + commands[c][sc][:3] + '" is an invalid option. MUST BE CONTAIN A HEX VALUE!')  # Error message
                                         sys.exit(0)  # End Script
-                num_of_conversions = int(len(commands[c]))  # find the number of commands in the second dimension array
-                for sc in range(0, num_of_conversions):  # runs for the number of commands in the second dimension array
 
+                for sc in range(0, len(commands[c])):  # runs for the number of commands in the second dimension array
                         index = commands[c][sc].find('$')  # search for '$' and hold the place value of the '$' in the current string, within the second dimension array
                         if index == 0:  # if a '$' was found and is at the beginning of the commands (to avoid conflict with hex value commands)
                                 commands[c][sc] = commands[c][sc][index + 1:]  # remove the '$' formatting
                                 commands[c][sc] = registers[int(commands[c][sc])]  # use the register commands as a reference in the 'registers' array and replace with the corresponding binary number
-
-                                # print(commands)  # prints the newly converted commands
                         else:  # if not a register value ($*), check for hex value
                                 index = commands[c][sc].find('0x')
-
                                 if index == 0:
                                         commands[c][sc] = commands[c][sc][index + 2:]  # remove the '$' formatting
-
                                         if len(commands[c][sc]) == 4:  # if the hex value if the default length
                                                 tmp_string_conv = ''  # Create a new string to be reference in the for loop, hold the converted command
-                                                num_of_hex_chars = int(len(commands[c][sc]))  # get the number of string chars in the secondary command
                                                 
-                                                for hv in range(0, num_of_hex_chars):  # run loop for the number of single hex value chars in the command
+                                                for hv in range(0, len(commands[c][sc])):  # run loop for the number of single hex value chars in the command
                                                         tmp_string = commands[c][sc][hv]   # the tmp_string holds the current single hex value to be converted
                                                         tmp_string_conv += hexval[tmp_string]  # Add the newly converted hex value to the rest of the converted values
 
                                                 commands[c][sc] = tmp_string_conv  # add the converted command to the array
-                                                # print(commands)  # prints the newly converted commands
                                         elif len(commands[c][sc]) == 2:  # if the hex value has a length of 2
                                                 if commands[c][0] == 'sll' or commands[c][0] == 'srl':  # check to see if rule  # 1 applies here
                                                         if commands[c][sc][0] == '0' or commands[c][sc][0] == '1':  # check to see if it starts with either a 0 or 1, to comply with rule  # 1
                                                                 tmp_string_conv = ''  # Create a new string to be reference in the for loop, hold the converted command
-                                                                num_of_hex_chars = int(len(commands[c][sc]))  # get the number of string chars in the secondary command
                                                                 
-                                                                for hv in range(0, num_of_hex_chars):  # run loop for the number of single hex value chars in the command
+                                                                for hv in range(0, len(commands[c][sc])):  # run loop for the number of single hex value chars in the command
                                                                         tmp_string = commands[c][sc][hv]   # the tmp_string holds the current single hex value to be converted
                                                                         tmp_string_conv += hexval[tmp_string]  # Add the newly converted hex value to the rest of the converted values
 
                                                                 commands[c][sc] = tmp_string_conv  # add the converted command to the array
-                                                                # print(commands)  # prints the newly converted commands
                                                         else:  # if sll or srl encoder's hex value does not have the proper format. Post error message and end the script
                                                                 print('Bad "' + commands[c][0] + '" formatting, on line ' + str(c + 1) + '. "' + commands[c][sc][0] + '" is an invalid option. MUST BE A "0" or "1"!')  # Error message
                                                                 sys.exit(0)  # End Script
@@ -146,12 +140,10 @@ def to_binary_converter():
                                                         tmp_string_reg = commands[c][sc][4:]  # tmp_string equals the register command
                                                         tmp_string_reg = tmp_string_reg.replace(commands[c][sc][5:6], '')  # remove the '$' before the register number
                                                         tmp_string_reg = tmp_string_reg.replace(commands[c][sc][6:7], registers[int(commands[c][sc][6:7])])  # replace the register number with the binary equivalent
-                                                        # commands[c][sc] = commands[c][sc].replace(commands[c][sc][4:], tmp_string_reg)
                                                         
                                                         # Hex value conversion
                                                         tmp_string_conv = ''  # Create a new string to be reference in the for loop, hold the converted command
-                                                        num_of_hex_chars = int(len(commands[c][sc])) - 4  # len(tmp_string_reg)  # get the number of string chars in the secondary command
-                                                        for hv in range(0, num_of_hex_chars):  # run loop for the number of single hex value chars in the command
+                                                        for hv in range(0, len(commands[c][sc]) - 4):  # run loop for the number of single hex value chars in the command
                                                                 tmp_string = commands[c][sc][hv]   # the tmp_string holds the current single hex value to be converted
                                                                 tmp_string_conv += hexval[tmp_string]  # Add the newly converted hex value to the rest of the converted values
 
