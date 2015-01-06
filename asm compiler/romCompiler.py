8bc1493e6d2001892801c6812dddad8b4960f0b5
spacelad101 <noahburrell0@gmail.com>
2015-01-06 11:07:58 -0500

    stuff

--- asm compiler/romCompiler.py
+++ asm compiler/romCompiler.py
@@ -2,16 +2,23 @@ __author__ = 'Tecnoman5000'
 def rom_main(rom_file_name, hex_file_name):
 	hex_file_path = "output/" + hex_file_name
 	hex_arr = []
+	final_array = []
 	num_lines = 0
+	address = 0
 
 	with open(hex_file_path, 'r') as hex_file:  # Open file
 		for line in hex_file.read().splitlines():  # For lines in hex file pull string data
 			hex_arr.append(line)
 			num_lines += 1
-	for index in range(0, len(hex_arr)):
-		print(hex_arr[index])
+
 	print(num_lines)
 
-	for index in range(0, int(len(hex_arr))/2):
-			print (hex_arr[index])
+	for index in range(0, len(hex_arr)):
+		print (hex_arr[index])
+		final_array.append(hex_arr[index])
+		final_array.append(" 0")
+		final_array.append(" 0")
+		final_array.append(" 0 ")
 
+	for index in range(0, len(final_array)):
+		print (final_array[index])
